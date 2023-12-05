f5858bd977c62a197e1b47f4cb1ad8674dd095f2
Meryl Lou Cabio <meryllou7@gmail.com>
2023-12-05 16:22:56 -0600

    Completed last two functions

--- markov.py
+++ markov.py
@@ -46,8 +46,28 @@ def make_chains(text_string):
     #split words by white space
     words = text_string.split()
 
-    #for loop to loop over list of words
-    
+    #for loop to loop over list of words by index
+    for i in range(len(words)-2):
+        #we need to add pair_words (keys) to dictionary chains
+        pair_words = (words[i], words[i+1])
+        #we created an empty values list
+        ##########when we do this it creates an empty list each for loop. we dont want that#######
+        # chains[pair_words] = []
+        value_word = words[i+2]
+
+    # #seperate for loop for the value - so itll keep looping and adding to the same list
+    # for pair_words in chains:
+    #     print(pair_words)
+        #Check to see if the key is in the dictionary already. if so add value
+        if pair_words in chains:
+            chains[pair_words].append(value_word)
+        #initialize that list and put your word into it
+        else:
+            chains[pair_words] = [value_word]
+
+        # value_words = words[i+2]
+        #dictionary[key] = [value]
+    # print(chains)
 
     # your code goes here
 
@@ -56,12 +76,37 @@ def make_chains(text_string):
 
 def make_text(chains):
     """Return text from chains."""
-
     words = []
 
-    # your code goes here
+    #get random key from dictionary -- will use choice and list(chains.keys())
+    key = choice(list(chains.keys()))
+    #will add each key to the lst
+    #we could also do words.extend(key)
+    for k in key:
+        words.append(k)
 
-    return ' '.join(words)
+    #get random value from dictionary[key]
+    # rand_value = choice(chains[key])
+    #will add to lst
+    # words.append(rand_value)
+
+    #repeat at random based on previous key until KeyError
+    while key in chains:
+
+        #1. look up new_key in chains and pull random value that matches new key
+        rand_value = choice(chains[key])
+        words.append(rand_value)
+
+        #2. make a new key out of key[1], rand_value
+        key = key[1], rand_value
+
+
+
+        #3. keep doing that
+    
+
+    print(words)
+    # return ' '.join(words)
 
 
 input_path = 'green-eggs.txt'
@@ -79,4 +124,4 @@ chains = make_chains(input_text)
 # Produce random text
 random_text = make_text(chains)
 
-print(random_text)
+# print(random_text)
